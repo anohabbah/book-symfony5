407b8d8373c6f0701f6c7bb3c1f030c0138045bf
Abbah <anohabbah@gmail.com>
2020-11-01 22:02:49 +0100

    Add easter egg

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,12 +11,19 @@ class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="home_page")
+     * @param Request $request
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s !</h1>', htmlspecialchars($name));
+        }
         return new Response(<<<EOF
 <html>
     <body>
+      $greet
       <img src="/images/under-construction.gif" />
     </body>
 </html>
