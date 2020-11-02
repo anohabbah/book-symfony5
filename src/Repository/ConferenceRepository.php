575c1eacffed7eab0abd317931926c876f876d2c
Abbah <anohabbah@gmail.com>
2020-11-02 13:31:59 +0100

    Override findAll method in ConferenceRepository
    Order the result by year and city

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Conference|null find($id, $lockMode = null, $lockVersion = null)
  * @method Conference|null findOneBy(array $criteria, array $orderBy = null)
- * @method Conference[]    findAll()
  * @method Conference[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ConferenceRepository extends ServiceEntityRepository
@@ -47,4 +46,12 @@ class ConferenceRepository extends ServiceEntityRepository
         ;
     }
     */
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findAll(): array
+    {
+        return $this->findBy([], ['year' => 'ASC', 'city' => 'ASC']);
+    }
 }
