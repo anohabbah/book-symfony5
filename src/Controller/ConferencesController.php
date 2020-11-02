50a78317eb8cefc6fcb9d8bfd4851312867f7df9
Abbah <anohabbah@gmail.com>
2020-11-02 15:00:01 +0100

    Display the comment form

--- src/Controller/ConferencesController.php
+++ src/Controller/ConferencesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -51,6 +53,9 @@ class ConferencesController extends AbstractController
         Conference $conference,
         CommentRepository $commentRepository
     ): Response {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -59,6 +64,7 @@ class ConferencesController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::ITEMS_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::ITEMS_PER_PAGE),
+            'comment_form' => $form->createView(),
         ]));
     }
 }
