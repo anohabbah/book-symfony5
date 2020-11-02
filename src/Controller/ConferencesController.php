3081fbb61b0da1c150cd474e506b7d01005ebb2d
Abbah <anohabbah@gmail.com>
2020-11-02 11:50:28 +0100

    Add paginator to comments

--- src/Controller/ConferencesController.php
+++ src/Controller/ConferencesController.php
@@ -6,6 +6,7 @@ use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -30,17 +31,27 @@ class ConferencesController extends AbstractController
     /**
      * @Route("/conferences/{id}", name="conference.show")
      *
+     * @param Request           $request           Request
      * @param Environment       $twig              Twig environment
      * @param Conference        $conference        Conference to show
      * @param CommentRepository $commentRepository Comment repository
      *
      * @return Response Outgoing response
      */
-    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
-    {
+    public function show(
+        Request $request,
+        Environment $twig,
+        Conference $conference,
+        CommentRepository $commentRepository
+    ): Response {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
         return new Response($twig->render('conferences/show.html.twig', [
             'conference' => $conference,
-            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::ITEMS_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::ITEMS_PER_PAGE),
         ]));
     }
 }
