9981b1f0c5f18900d0d78c9e568f9c20e97f0ae1
Abbah <anohabbah@gmail.com>
2020-11-04 01:05:25 +0100

    Test emails sending

--- tests/Controller/ConferencesControllerTest.php
+++ tests/Controller/ConferencesControllerTest.php
@@ -54,4 +54,17 @@ class ConferencesControllerTest extends PantherTestCase
         self::assertSelectorTextContains('h2', 'Amsterdam 2019');
         self::assertSelectorExists('div:contains("There are 1 comments")');
     }
+
+    public function testMailerAssertions(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        self::assertEmailCount(1);
+        $event = self::getMailerEvent(0);
+        self::assertEmailIsQueued($event);
+        $email = self::getMailerMessage(0);
+        self::assertEmailHeaderSame($email, 'To', 'fabien@example.com');
+        self::assertEmailTextBodyContains($email, 'Bar');
+        self::assertEmailAttachmentCount($email, 1);
+    }
 }
