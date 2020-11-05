0d49547ff976d4c2e490e36775968ce35a92d046
Abbah <anohabbah@gmail.com>
2020-11-05 13:01:32 +0100

    Implement ChatNotificationInterface to override the default rendering of the Slack message

--- src/Notification/CommentReviewNotification.php
+++ src/Notification/CommentReviewNotification.php
@@ -3,12 +3,17 @@
 namespace App\Notification;
 
 use App\Entity\Comment;
+use Symfony\Component\Notifier\Bridge\Slack\Block\SlackDividerBlock;
+use Symfony\Component\Notifier\Bridge\Slack\Block\SlackSectionBlock;
+use Symfony\Component\Notifier\Bridge\Slack\SlackOptions;
+use Symfony\Component\Notifier\Message\ChatMessage;
 use Symfony\Component\Notifier\Message\EmailMessage;
+use Symfony\Component\Notifier\Notification\ChatNotificationInterface;
 use Symfony\Component\Notifier\Notification\EmailNotificationInterface;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\Recipient\Recipient;
 
-class CommentReviewNotification extends Notification implements EmailNotificationInterface
+class CommentReviewNotification extends Notification implements EmailNotificationInterface, ChatNotificationInterface
 {
     /**
      * @var Comment
@@ -17,6 +22,7 @@ class CommentReviewNotification extends Notification implements EmailNotificatio
 
     /**
      * CommentReviewNotification constructor.
+     *
      * @param Comment $comment Comment posted
      */
     public function __construct(Comment $comment)
@@ -31,8 +37,47 @@ class CommentReviewNotification extends Notification implements EmailNotificatio
         $message = EmailMessage::fromNotification($this, $recipient, $transport);
         $message->getMessage()
             ->htmlTemplate('emails/comments/notification.html.twig')
-            ->context(['comment' => $this->comment])
-        ;
+            ->context(['comment' => $this->comment]);
+
+        return $message;
+    }
+
+    public function getChannels(Recipient $recipient): array
+    {
+        if (preg_match('{\b(great|awesome)\b}i', $this->comment->getText())) {
+            return ['email', 'chat/slack'];
+        }
+
+        $this->importance(Notification::IMPORTANCE_LOW);
+
+        return ['email'];
+    }
+
+    public function asChatMessage(Recipient $recipient, string $transport = null): ?ChatMessage
+    {
+        if ('slack' !== $transport) {
+            return null;
+        }
+
+        $message = ChatMessage::fromNotification($this/*, $recipient*/);
+        $message->subject($this->getSubject());
+        $message->transport($transport);
+        $message->options((new SlackOptions())
+            ->iconEmoji('tada')
+            ->iconUrl('https://guestbook.example.com')
+            ->username('Guestbook')
+            ->block((new SlackSectionBlock())->text($this->getSubject()))
+            ->block(new SlackDividerBlock())
+            ->block(
+                (new SlackSectionBlock())
+                    ->text(sprintf(
+                        '%s (%s) says: %s',
+                        $this->comment->getAuthor(),
+                        $this->comment->getEmail(),
+                        $this->comment->getText()
+                    ))
+            )
+        );
 
         return $message;
     }
